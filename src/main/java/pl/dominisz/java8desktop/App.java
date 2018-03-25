cd99b53c7e631d6e9d616551aeb50deb00be3bcb
dominisz <dominisz@gmail.com>
2018-03-25 19:00:40 +0200

    Initial commit

---
+++ src/main/java/pl/dominisz/java8desktop/App.java
@@ -0,0 +1,13 @@
+package pl.dominisz.java8desktop;
+
+/**
+ * http://dominisz.pl
+ * 25.03.2018
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
---
+++ src/test/java/pl/dominisz/java8desktop/AppTest.java
@@ -0,0 +1,21 @@
+package pl.dominisz.java8desktop;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * http://dominisz.pl
+ * 25.03.2018
+ */
+class AppTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void main() {
+    }
+}
