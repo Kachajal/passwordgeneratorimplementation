801f4e431728550bb626a8ebf8cabc52c420853a
Konrad S <konrad.t.szmit@gmail.com>
2019-03-18 20:41:23 +0100

    Added lowercase password generation.

--- src/main/java/pl/umcs/passwordgeneratorimplementation/PasswordGeneratorImplementation.java
+++ src/main/java/pl/umcs/passwordgeneratorimplementation/PasswordGeneratorImplementation.java
@@ -4,15 +4,21 @@ import pl.umcs.passwordgeneratorapi.PasswordGeneratorParameters;
 import pl.umcs.passwordgeneratorapi.PasswordGeneratorService;
 
 import java.util.List;
+import java.util.Random;
 
 public class PasswordGeneratorImplementation implements PasswordGeneratorService {
 
+    private static final Random RANDOM = new Random();
+    private static String LOWERCASE_ALPHABET = "abcdefghijklmnopqrstuvwxyz";
     public String generate(PasswordGeneratorParameters passwordGeneratorParameters) {
-        return null;
+        String generatedPassword = new String();
+        for (int i = 0; i < passwordGeneratorParameters.getPasswordLength(); i++) {
+            generatedPassword += (LOWERCASE_ALPHABET.charAt(RANDOM.nextInt(LOWERCASE_ALPHABET.length())));
+        }
+        return generatedPassword;
     }
 
     public List<String> generate(PasswordGeneratorParameters passwordGeneratorParameters, int count) {
         return null;
-
     }
 }
