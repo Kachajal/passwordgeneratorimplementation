ed4e1cde08ac1ca70ebf48d20baa80327ade356f
Konrad S <konrad.t.szmit@gmail.com>
2019-03-18 19:40:08 +0100

    Zad11

--- src/main/java/pl/umcs/java8desktop/App.java
+++ src/main/java/pl/umcs/java8desktop/App.java
@@ -1,9 +1,6 @@
-package pl.dominisz.java8desktop;
+package pl.umcs.java8desktop;
 
-/**
- * http://dominisz.pl
- * 25.03.2018
- */
+
 public class App {
 
     public static void main(String[] args) {
