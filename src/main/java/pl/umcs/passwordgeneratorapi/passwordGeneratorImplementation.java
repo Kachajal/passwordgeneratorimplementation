d118328e835cfa11dd6b8e468843a9a8e1a8b841
Konrad S <konrad.t.szmit@gmail.com>
2019-03-18 20:09:14 +0100

    Initial commit.

---
+++ src/main/java/pl/umcs/passwordgeneratorapi/passwordGeneratorImplementation.java
@@ -0,0 +1,14 @@
+package pl.umcs.passwordgeneratorapi;
+
+import java.util.List;
+
+public class passwordGeneratorImplementation implements PasswordGeneratorService {
+
+    public String generate(PasswordGeneratorParameters passwordGeneratorParameters) {
+        return null;
+    }
+
+    public List<String> generate(PasswordGeneratorParameters passwordGeneratorParameters, int count) {
+        return null;
+    }
+}
